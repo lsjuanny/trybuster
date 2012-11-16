68d61cfccf6f628a8c7d7232a9f61b43c93f795e
Juan Shi <shijuan@angryequal-lm.peking.corp.yahoo.com>
2012-11-16 14:53:45 +0800

    come on, fix the problem

--- coverage/coverage-reporter.js
+++
@@ -1,9 +0,0 @@
-if (typeof module === "object" && typeof require === "function") {
-    var buster = require("buster-test");
-}
-
-buster.testRunner.onCreate(function (runner) {
-    runner.on('suite:end', function () {
-        buster.emit('coverage:report', _$Coverage);
-    });
-});
